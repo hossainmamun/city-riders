f0fead9340a1469003d262a78bed1a74cd2be17e
hossainmamun <hossainmamun337@gmailcom>
2021-07-06 13:52:10 +0600

    something change

--- src/Component/Destination/Destination.js
+++ src/Component/Destination/Destination.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Map from '../Map/Map.js';
 
 const Destination = () => {
-    
+
     return (
         <div className="destination">
-            <h1>this is destination</h1>
+            <h1>This Is The Destination Page</h1>
+            <h1>sorry for this part is not available</h1>
             <Map></Map>
         </div>
     )
